3dd7a20ddb9ee3279b53dc0983337fe3c577142e
canmertinyo <squalcan@gmail.com>
2023-05-31 22:24:35 +0300

    delete options database file

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -1,15 +1,13 @@
 import mongoose from 'mongoose';
-import { options } from './dbOptions';
 import { logger } from '../utils/logger';
 
 export class DatabaseConfiguration {
-  constructor(private readonly dbUri: string, public mongooseOptions?: object) {
-    this.mongooseOptions = options;
+  constructor(private readonly dbUri: string) {
     this.databaseConnection();
   }
   public async databaseConnection() {
     return mongoose
-      .connect(this.dbUri, this.mongooseOptions)
+      .connect(this.dbUri)
       .then(() => {
         console.log('Successfully connected!');
         logger.log('info', 'Succesfully connected to the database');
--- backend/src/database/dbOptions.ts
+++
@@ -1,8 +0,0 @@
-//If you want to change any options from mongoose. I wrote must important options down there
-
-export const options = {
-  authSource: 'admin',
-  useNewUrlParser: true,
-  socketTimeoutMS: 30000,
-  connectTimeoutMS: 30000,
-};
