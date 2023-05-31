30d56dd7baf13e3c1deac1178d4a3f68b6175dbd
canmertinyo <squalcan@gmail.com>
2023-05-31 22:56:16 +0300

    Add timestamp from winston

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -1,8 +1,9 @@
-import winston from 'winston';
+import winston, { format } from 'winston';
+const { combine, label, prettyPrint, timestamp } = format;
 
 export const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: combine(label({ label: 'Log' }), timestamp(), prettyPrint()),
   transports: [
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.File({ filename: 'combined.log' }),
