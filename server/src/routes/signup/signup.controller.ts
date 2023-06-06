0c68e7145d4120ef0b9c710ec60b96c76cfec0d9
canmertinyo <squalcan@gmail.com>
2023-06-07 00:19:50 +0300

    🐱‍👤 update validation & controller
    🟢 add: trim for validation to avoid any whitespace inputs,
    🟢 add : response type and cover with try catch block . To catch potential errors

--- server/src/routes/signup/signup.controller.ts
+++ server/src/routes/signup/signup.controller.ts
@@ -1,15 +1,25 @@
 import { Request, Response } from "express";
 import { ModifiedUser } from "../../utils/interface/modified.user.type";
 import { signUser } from "./signup.service";
+import { HttpStatusCode } from "../../core/api.response";
 
 // const signupService = new SignupService();
 
-export const signupUser = async (req: Request, res: Response) => {
-  const { email, password, username } = req.body;
+export const signupUser = async (
+  req: Request,
+  res: Response
+): Promise<Response<any, Record<string, any>>> => {
+  try {
+    const { email, password, username } = req.body;
 
-  const user: ModifiedUser = { email, password, username };
+    const user: ModifiedUser = { email, password, username };
 
-  const currentUser: any = signUser(user);
+    const currentUser = await signUser(user);
 
-  return res.json({ currentUser });
+    return res
+      .status(HttpStatusCode.CREATED)
+      .json({ status: "created", userInfo: user });
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
 };
--- server/src/routes/signup/signup.validation.ts
+++ server/src/routes/signup/signup.validation.ts
@@ -4,11 +4,12 @@ export class SignupValidation {
   public signup() {
     try {
       const schema = Joi.object({
-        username: Joi.string().alphanum().min(3).max(12).required(),
+        username: Joi.string().alphanum().min(3).max(12).required().trim(),
         password: Joi.string()
           .required()
-          .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
-        email: Joi.string().required().email(),
+          .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+          .trim(),
+        email: Joi.string().required().email().trim(),
       });
       return schema;
     } catch (error: any) {
