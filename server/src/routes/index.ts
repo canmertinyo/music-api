0b5b32860e571acbd143492a0a4644f483f62bbc
canmertinyo <squalcan@gmail.com>
2023-06-07 16:23:19 +0300

    📦 tweaked controller and route index

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,17 +4,26 @@ import GenerateRouter from "./router.factory";
 import { SignupValidation } from "./signup/signup.validation";
 import { validateSchema } from "../core/middlewares/schema.factory";
 import ValidateLogin from "./login/login.validation";
-import { loginUserController } from "./login/login.controller";
+import { LoginController } from "./login/login.controller";
 import { Container } from "../core/decorators/dependency.injection";
 
 export const expressRouter = express.Router();
 
+//Container ----------------------------------------------------------------------------------------
 const container = new Container();
 container.register("ValidateLogin", new ValidateLogin());
 container.register("SignupValidation", new SignupValidation());
+container.register("LoginController", new LoginController());
+// container.register("GenerateRouter", new GenerateRouter(expressRouter));
+//Container ----------------------------------------------------------------------------------------
+
+//Resolve ----------------------------------------------------------------------------------------
 const validateLogin = container.resolve<ValidateLogin>("ValidateLogin");
 const signupValidation =
   container.resolve<SignupValidation>("SignupValidation");
+const loginController = container.resolve<LoginController>("LoginController");
+// const generateRouter = container.resolve<Ge>
+//Container ----------------------------------------------------------------------------------------
 
 const generateRouter = new GenerateRouter(expressRouter);
 generateRouter.POST(
@@ -25,5 +34,5 @@ generateRouter.POST(
 generateRouter.POST(
   "/login",
   validateSchema(validateLogin.login()),
-  loginUserController
+  loginController.login()
 );
--- server/src/routes/login/login.controller.ts
+++ server/src/routes/login/login.controller.ts
@@ -1,25 +1,30 @@
 import { Request, Response } from "express";
 import LoginService from "./login.service";
 import { HttpStatusCode } from "../../utils/status_codes/api.response";
-import { Container } from "../../core/decorators/dependency.injection";
+import { Container, Inject } from "../../core/decorators/dependency.injection";
 
 const container = new Container();
 container.register("LoginService", new LoginService());
 const loginService = container.resolve<LoginService>("LoginService");
 
-export const loginUserController = async (
-  req: Request,
-  res: Response
-): Promise<Response<any, Record<string, any>>> => {
-  try {
-    const { email, password } = req.body;
-
-    const currentUser = await loginService.loginUser(email, password);
-
-    return res
-      .status(HttpStatusCode.CREATED)
-      .json({ status: "created", userInfo: currentUser });
-  } catch (error: any) {
-    throw new Error(error.message);
+@Inject()
+export class LoginController {
+  constructor() {
+    this.login;
   }
-};
+  public login() {
+    return async (req: Request, res: Response) => {
+      try {
+        const { email, password } = req.body;
+
+        const currentUser = await loginService.loginUser(email, password);
+
+        return res
+          .status(HttpStatusCode.CREATED)
+          .json({ status: "created", userInfo: currentUser });
+      } catch (error: any) {
+        throw new Error(error.message);
+      }
+    };
+  }
+}
