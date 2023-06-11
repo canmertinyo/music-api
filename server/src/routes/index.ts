a2bf845f647c9514d4c709ad5b9bf056f9ba5e05
canmertinyo <squalcan@gmail.com>
2023-06-11 17:22:00 +0300

    🟢 prettierc update

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -10,24 +10,10 @@ import { resolveContainerFactory } from "../core/decorators/dependency.factory";
 export const expressRouter = express.Router();
 
 const validateLogin = resolveContainerFactory("ValidateLogin", ValidateLogin);
-const loginController = resolveContainerFactory(
-  "LoginController",
-  LoginController
-);
+const loginController = resolveContainerFactory("LoginController", LoginController);
 const signupController = resolveContainerFactory("signupUser", signupUser);
-const signupValidation = resolveContainerFactory(
-  "SignupValidation",
-  SignupValidation
-);
+const signupValidation = resolveContainerFactory("SignupValidation", SignupValidation);
 
 const generateRouter = new GenerateRouter(expressRouter);
-generateRouter.POST(
-  "/signup",
-  validateSchema(signupValidation.signup()),
-  signupController
-);
-generateRouter.POST(
-  "/login",
-  validateSchema(validateLogin.login()),
-  loginController.login()
-);
+generateRouter.POST("/signup", validateSchema(signupValidation.signup()), signupController);
+generateRouter.POST("/login", validateSchema(validateLogin.login()), loginController.login());
