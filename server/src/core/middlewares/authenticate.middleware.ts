ad00f59ca8180e5e25b663ef9c22effabe5c6276
canmertinyo <squalcan@gmail.com>
2023-06-11 17:12:24 +0300

    📦 refactor : authenticate middleware
    🐱‍👤 Removed unnecessary blocks and improved code readability.

--- server/src/core/middlewares/authenticate.middleware.ts
+++ server/src/core/middlewares/authenticate.middleware.ts
@@ -1,35 +1,29 @@
 import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { Jwt } from "jsonwebtoken";
 import { verifyToken } from "../jwt/token";
 import { UserModel } from "../../model/user.model";
+import { JwtInvalidTokenException, JwtUserNotFoundException } from "../../exceptions";
 
-export async function authenticate(
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<Response | void> {
+export async function authenticate(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
   const bearer = req.headers.authorization;
-  if (!bearer || !bearer.startsWith("Bearer")) {
-    return next();
-  }
+  if (!bearer || !bearer.startsWith("Bearer")) return next();
 
-  const accessToken = bearer.split("Bearer: ")[1].trim();
+  const accessToken = bearer.split("Bearer")[1].trim();
 
   try {
     const payload: any = await verifyToken(accessToken);
 
     if (payload instanceof jwt.JsonWebTokenError) {
-      return next(new Error("UNAUTHORIZED"));
+      return next(new JwtInvalidTokenException("Invalid Token"));
     }
 
-    const user = await UserModel.findById(payload.id)
-      .select("-password")
-      .exec();
+    const user = await UserModel.findById(payload.id).select("-password").exec();
 
     if (!user) {
-      return next(new Error("UNAUTHORIZED"));
+      return next(new JwtUserNotFoundException("User not found!"));
     }
     req.user = user;
+    return next();
   } catch (error: any) {
     return next(new Error(error.message));
   }
